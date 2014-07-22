f3f92c52acb5d8d2556cdefee75e243f1f0ff0b8
Rudi Giesler <rudi@praekelt.com>
2014-07-22 16:16:48 +0200

    Created dummy menu without dictionary meanings.

--- src/app.js
+++ src/app.js
@@ -1,30 +1,34 @@
 go.app = function() {
     var vumigo = require('vumigo_v02');
     var App = vumigo.App;
-    var Choice = vumigo.states.Choice;
-    var ChoiceState = vumigo.states.ChoiceState;
+    //var Choice = vumigo.states.Choice;
+    //var ChoiceState = vumigo.states.ChoiceState;
+    var FreeText = vumigo.states.FreeText;
     var EndState = vumigo.states.EndState;
 
     var GoApp = App.extend(function(self) {
         App.call(self, 'states:start');
 
         self.states.add('states:start', function(name) {
-            return new ChoiceState(name, {
-                question: 'Hi there! What do you want to do?',
+            return new FreeText(name, {
+                question: [
+                    'Hi there! What word would you like the definition for?'
+                ].join(''),
 
-                choices: [
-                    new Choice('states:start', 'Show this menu again'),
-                    new Choice('states:end', 'Exit')],
-
-                next: function(choice) {
-                    return choice.value;
+                next: function(content) {
+                    return {
+                        name: 'states:end',
+                        creator_opts: {
+                            query: content
+                        }
+                    };
                 }
             });
         });
 
-        self.states.add('states:end', function(name) {
+        self.states.add('states:end', function(name, opts) {
             return new EndState(name, {
-                text: 'Thanks, cheers!',
+                text: 'Definition of ' + opts.query + ": Not yet implemented.",
                 next: 'states:start'
             });
         });
